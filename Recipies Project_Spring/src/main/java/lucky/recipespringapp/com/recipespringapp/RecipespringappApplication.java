2c7f72d0a20cedfb84438d210644bc2d4075bd01
=SiddharthLucky <=siddharth.luckyreddy@gmail.com>
2018-07-19 15:55:58 -0400

    Initial commit

---
+++ Recipies Project_Spring/src/main/java/lucky/recipespringapp/com/recipespringapp/RecipespringappApplication.java
@@ -0,0 +1,12 @@
+package lucky.recipespringapp.com.recipespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipespringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipespringappApplication.class, args);
+    }
+}

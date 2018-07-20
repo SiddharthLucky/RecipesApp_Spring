0dda9d4137927e820e392a35c16b79c3632c8bea
=SiddharthLucky <=siddharth.luckyreddy@gmail.com>
2018-07-20 11:01:41 -0400

    Made a Initial table structure

---
+++ Recipies Project_Spring/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO uom (description) VALUES ('Teaspoon');
+INSERT INTO uom (description) VALUES ('Tablespoon');
+INSERT INTO uom (description) VALUES ('Cup');
+INSERT INTO uom (description) VALUES ('Pinch');
+INSERT INTO uom (description) VALUES ('Ounce');
